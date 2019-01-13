cba75e47c21b0d01856a0e29d16ada9f1fd658dc
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-01-13 22:16:46 +0900

    add : [day4] harib01f

---
+++ day4/harib01f/bootpack.c
@@ -0,0 +1,73 @@
+/*
+This linker script is created by refer following page.
+http://takeisamemo.blogspot.com/2014/09/os30os-3-7.html
+*/
+
+void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
+
+void HariMain(void){
+    int i;
+    char *p;
+    
+    init_palette();
+    
+    p = (char *)0xa0000;
+    
+    for(i = 0; i <= 0xffff; i++){
+        p[i] = i & 0x0f;
+    }
+    
+    for(;;){
+        io_hlt();
+    }
+}
+
+void init_palette(void){
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00,
+        0xff, 0x00, 0x00,
+        0x00, 0xff, 0x00,
+        0xff, 0xff, 0x00,
+        0x00, 0x00, 0xff,
+        0xff, 0x00, 0xff,
+        0x00, 0xff, 0xff,
+        0xff, 0xff, 0xff,
+        0xc6, 0xc6, 0xc6,
+        0x84, 0x00, 0x00,
+        0x00, 0x84, 0x00,
+        0x84, 0x84, 0x00,
+        0x00, 0x00, 0x84,
+        0x84, 0x00, 0x84,
+        0x00, 0x84, 0x84,
+        0x84, 0x84, 0x84
+    };
+    
+    set_palette(0, 15, table_rgb);
+    return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb){
+    int i;
+    int eflags;
+    
+    eflags = io_load_eflags(); //割り込み許可フラグの値を記録
+    io_cli(); //割り込み禁止(許可フラグを0にする)
+    io_out8(0x03c8, start);
+    
+    for(i = start; i <= end; i++){
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    
+    io_store_eflags(eflags); //割り込み許可フラグを戻す
+    return;
+}

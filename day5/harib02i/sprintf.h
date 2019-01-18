6071427c32d81154b682e2982c0505cc4f8670ad
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-01-18 21:45:11 +0900

    add : [day5] harib02i

---
+++ day5/harib02i/sprintf.h
@@ -0,0 +1,6 @@
+void *sprintf(char *s, char *format, ...);
+unsigned int to_dec_asc(char *buf, int n);
+unsigned int to_hex_asc(char *buf, int n);
+unsigned int ndigit(unsigned int n);
+unsigned int upow(unsigned int x, unsigned int n);
+

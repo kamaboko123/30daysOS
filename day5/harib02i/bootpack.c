c9eae3193d1b86ae006dd697a3b30b65546fb0f8
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-01-20 21:47:29 +0900

    fix : GDT limit_high caliculation

--- day5/harib02i/bootpack.c
+++ day5/harib02i/bootpack.c
@@ -109,7 +109,7 @@ void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, i
     sd->base_low = base & 0xffff;
     sd->base_mid = (base >> 16) & 0xff;
     sd->access_right = ar & 0xff;
-    sd->limit_high = (((limit >> 16) & 0xff) | (ar >> 8) & 0xf0);
+    sd->limit_high = (((limit >> 16) & 0x0f) | (ar >> 8) & 0xf0);
     sd->base_high = (base >> 24) & 0xff;
 }
 

f0e473319e6187ea94adcc4a258c00ffe982c293
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-01-31 22:44:46 +0900

    update test code for stdlibc

--- tools/stdlibc/src/test/main.c
+++ tools/stdlibc/src/test/main.c
@@ -3,6 +3,9 @@
 #include "stdlibc.h"
 
 void test_isdigit(void);
+void test_iscapital(void);
+void test_upow(void);
+void test_atoi(void);
 
 int main(){
     CU_pSuite testSuite;
@@ -10,7 +13,10 @@ int main(){
     CU_initialize_registry();
     testSuite = CU_add_suite("StdlibcTest", NULL, NULL);
     
-    CU_add_test(testSuite, "test of isdigit()", test_isdigit);
+    CU_add_test(testSuite, "test of _isdigit()", test_isdigit);
+    CU_add_test(testSuite, "test of _iscapital()", test_iscapital);
+    CU_add_test(testSuite, "test of _upow()", test_upow);
+    CU_add_test(testSuite, "test of _atoi()", test_atoi);
     
     CU_basic_run_tests();
     CU_cleanup_registry();
@@ -18,9 +24,30 @@ int main(){
     return 0;
 }
 
-void test_isdigit(void){
+void test_upow(void){
+    CU_ASSERT(8 == _upow(2, 3));
+    CU_ASSERT(10000 == _upow(10, 4));
+    CU_ASSERT(4096 == _upow(16, 3));
+}
+
+void test_iscapital(){
+    CU_ASSERT(_iscapital('A') == TRUE);
+    CU_ASSERT(_iscapital('Z') == TRUE);
+    CU_ASSERT(_iscapital('0') == FALSE);
+    CU_ASSERT(_iscapital('9') == FALSE);
+}
+
+void test_isdigit(){
     CU_ASSERT(_isdigit('A') == FALSE);
     CU_ASSERT(_isdigit('Z') == FALSE);
     CU_ASSERT(_isdigit('0') == TRUE);
     CU_ASSERT(_isdigit('9') == TRUE);
 }
+
+void test_atoi(){
+    CU_ASSERT(1234567890 == _atoi("1234567890"));
+    CU_ASSERT(12345 == _atoi("12345a67890"));
+    CU_ASSERT(0 == _atoi("a1234567890"));
+}
+
+

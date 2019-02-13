5c7f1c9b79d26ae477abe5ea339c3441d273bfa8
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-02-13 22:47:52 +0900

    add : [day10] harib07a

--- day10/harib07a/include/bootpack.h
+++ day10/harib07a/include/bootpack.h
@@ -25,8 +25,13 @@ struct MOUSE_DEC{
 };
 
 //bootpack.c
-#define MEMMAN_ADDR 0x003c0000
-#define MEMMAN_FREES 4090
+
+
+//memory.c
+#define EFLAGS_AC_BIT 0x00040000 //18(AC) bit
+#define CR0_CACHE_DISABLE 0x60000000 //30(CD), 21(PG) bit
+#define MEMMAN_ADDR 0x003c0000 //メモリ管理情報を置くアドレス
+#define MEMMAN_FREES 4090 //メモリの空き領域リストの最大個数
 
 struct FREEINFO{
     unsigned int addr;
@@ -45,8 +50,10 @@ unsigned int memtest(unsigned int start, unsigned int end);
 unsigned int memtest_sub(unsigned int start, unsigned int end);
 void memman_init(struct MEMMAN *man);
 unsigned int memman_total(struct MEMMAN *man);
-unsigned int memmam_alloc(struct MEMMAN *man, unsigned int size);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
 int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
+unsigned int memman_alloc_4k(struct MEMMAN *man, unsigned int size);
+int memman_free_4k(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 
 //asm_func.s

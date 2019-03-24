7f3f6864e3a875a96bed8af83f988a021fd1a392
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-03-24 15:38:47 +0900

    fix fifo.c

--- day15/harib12e/src/fifo.c
+++ day15/harib12e/src/fifo.c
@@ -1,57 +1,87 @@
-/* FIFOライブラリ */
-
 #include "bootpack.h"
 
-#define FLAGS_OVERRUN		0x0001
-
-void fifo32_init(struct FIFO32 *fifo, int size, int *buf)
-/* FIFOバッファの初期化 */
-{
-	fifo->size = size;
-	fifo->buf = buf;
-	fifo->free = size; /* 空き */
-	fifo->flags = 0;
-	fifo->p = 0; /* 書き込み位置 */
-	fifo->q = 0; /* 読み込み位置 */
-	return;
+void fifo8_init(struct FIFO8 *fifo, int size, unsigned char *buf){
+    fifo->size = size;
+    fifo->buf;
+    fifo->free = size;
+    fifo->flags = 0;
+    fifo->p = 0;
+    fifo->q = 0;
 }
 
-int fifo32_put(struct FIFO32 *fifo, int data)
-/* FIFOへデータを送り込んで蓄える */
-{
-	if (fifo->free == 0) {
-		/* 空きがなくてあふれた */
-		fifo->flags |= FLAGS_OVERRUN;
-		return -1;
-	}
-	fifo->buf[fifo->p] = data;
-	fifo->p++;
-	if (fifo->p == fifo->size) {
-		fifo->p = 0;
-	}
-	fifo->free--;
-	return 0;
+int fifo8_put(struct FIFO8 *fifo, unsigned char data){
+    if(fifo->free == 0) {
+        fifo->flags |= FLAGS_OVERRUN;
+        return -1;
+    }
+    
+    fifo->buf[fifo->p] = data;
+    fifo->p++;
+    
+    fifo->p %= fifo->size;
+    fifo->free--;
 }
 
-int fifo32_get(struct FIFO32 *fifo)
-/* FIFOからデータを一つとってくる */
-{
-	int data;
-	if (fifo->free == fifo->size) {
-		/* バッファが空っぽのときは、とりあえず-1が返される */
-		return -1;
-	}
-	data = fifo->buf[fifo->q];
-	fifo->q++;
-	if (fifo->q == fifo->size) {
-		fifo->q = 0;
-	}
-	fifo->free++;
-	return data;
+int fifo8_get(struct FIFO8 *fifo){
+    int data;
+    
+    if(fifo->free == fifo->size){
+        return -1;
+    }
+    
+    data = fifo->buf[fifo->q];
+    fifo->q++;
+    
+    fifo->q %= fifo->size;
+    fifo->free++;
+    
+    return data;
 }
 
-int fifo32_status(struct FIFO32 *fifo)
-/* どのくらいデータが溜まっているかを報告する */
-{
-	return fifo->size - fifo->free;
+int fifo8_status(struct FIFO8 *fifo){
+    return fifo->size - fifo->free;
+}
+
+
+
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf){
+    fifo->size = size;
+    fifo->buf;
+    fifo->free = size;
+    fifo->flags = 0;
+    fifo->p = 0;
+    fifo->q = 0;
+}
+
+int fifo32_put(struct FIFO32 *fifo, int data){
+    if(fifo->free == 0) {
+        fifo->flags |= FLAGS_OVERRUN;
+        return -1;
+    }
+    
+    fifo->buf[fifo->p] = data;
+    fifo->p++;
+    
+    fifo->p %= fifo->size;
+    fifo->free--;
+}
+
+int fifo32_get(struct FIFO32 *fifo){
+    int data;
+    
+    if(fifo->free == fifo->size){
+        return -1;
+    }
+    
+    data = fifo->buf[fifo->q];
+    fifo->q++;
+    
+    fifo->q %= fifo->size;
+    fifo->free++;
+    
+    return data;
+}
+
+int fifo32_status(struct FIFO32 *fifo){
+    return fifo->size - fifo->free;
 }

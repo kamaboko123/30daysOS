b7ff57595d3f55c091db46a346c573f2ff4ebba1
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-04-17 00:42:55 +0900

    update [day22] harib19e

--- day22/harib19e/src/app/lib/haribote.s
+++ day22/harib19e/src/app/lib/haribote.s
@@ -17,6 +17,7 @@ api_putchar:
 .global api_putstr0
 #void api_putstr0(char *s)
 api_putstr0:
+    #cdeclではebxは呼び出し前後で変わってはいけない事になっているので、保存して戻す
     push %ebx
     movl $2, %edx
     movl 8(%esp), %ebx

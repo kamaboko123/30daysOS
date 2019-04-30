91ed208fad26d5197c1d4f70ac575726d5aa41c1
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-04-30 15:19:05 +0900

    add [day25] harib22d

---
+++ day25/harib22d/src/app/star1/star1.c
@@ -0,0 +1,13 @@
+#include "haribote.h"
+
+void HariMain(void){
+    char *buf;
+    int win;
+    
+    api_initmalloc();
+    buf = api_malloc(150 * 100);
+    win = api_openwin(buf, 150, 100, -1, "star1");
+    api_boxfillwin(win, 6, 26, 143, 93, 0); //黒
+    api_point(win, 75, 59, 3); //黄
+    api_end();
+}

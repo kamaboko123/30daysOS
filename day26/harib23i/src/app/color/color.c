8477cac920b96a56352397caa66eded45a754099
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-05-02 22:49:23 +0900

    add : [day26] harib23i

---
+++ day26/harib23i/src/app/color/color.c
@@ -0,0 +1,26 @@
+#include "haribote.h"
+#include "stdlibc.h"
+
+void HariMain(void){
+    char *buf;
+    int win;
+    int x, y;
+    int r, g, b;
+    
+    api_initmalloc();
+    buf = api_malloc(144 * 164);
+    win = api_openwin(buf, 144, 164, -1, "color");
+    
+    for(y = 0; y < 128; y++){
+        for(x = 0; x < 128; x++){
+            r = x * 2;
+            g = y * 2;
+            b = 0;
+            buf[(x + 8) + (y + 28) * 144] = 16 + (r / 43) + (g /43) * 6 + (b / 43) * 36;
+        }
+    }
+    api_refreshwin(win, 8, 28, 136, 156);
+    api_getkey(1);
+    api_closewin(win);
+    api_end();
+}

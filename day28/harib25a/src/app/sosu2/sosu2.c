787acf51da77f0d07377b624dcaefea5f9cda406
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-05-05 13:51:24 +0900

    update harib25a, add sosu2

---
+++ day28/harib25a/src/app/sosu2/sosu2.c
@@ -0,0 +1,25 @@
+#include "apilib.h"
+#include "stdlibc.h"
+
+#define MAX 10000
+
+void HariMain(void){
+    char flag[MAX];
+    char s[8];
+    int i, j;
+    
+    for(i = 0; i < MAX; i++){
+        flag[i] = 0;
+    }
+    for(i = 2; i < MAX; i++){
+        if(flag[i] == 0){
+            _sprintf(s, "%d ", i);
+            api_putstr0(s);
+            for(j = i * 2; j < MAX; j += i){
+                flag[j] = 1;
+            }
+        }
+    }
+    
+    api_end();
+}

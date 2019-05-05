cdec739fc9ac7e599a32c29cf0685a76c7047feb
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-05-05 23:08:42 +0900

    add : [day28] harib25e

---
+++ day28/harib25e/src/app/typeipl/typeipl.c
@@ -0,0 +1,14 @@
+#include "apilib.h"
+
+void HariMain(void){
+    int fh;
+    char c;
+    fh = api_fopen("ipl10.s");
+    if(fh != 0){
+        for(;;){
+            if(api_fread(&c, 1, fh) == 0) break;
+            api_putchar(c);
+        }
+    }
+    api_end();
+}

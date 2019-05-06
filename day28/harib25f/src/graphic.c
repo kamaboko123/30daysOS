cf3eea3229042696c913f0329eac17b6a8ea7cd4
kamaboko123 <6112062+kamaboko123@users.noreply.github.com>
2019-05-06 10:41:38 +0900

    fix  harib25f

--- day28/harib25f/src/graphic.c
+++ day28/harib25f/src/graphic.c
@@ -50,7 +50,6 @@ void putblock8_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py
     }
 }
 
-/*
 void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s){
     extern char hankaku[4096];
     struct TASK *task = task_now();
@@ -104,53 +103,6 @@ void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s
         }
     }
 }
-*/
-void putfonts8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s)
-{
-    extern char hankaku[4096];
-    struct TASK *task = task_now();
-    char *nihongo = (char *) *((int *) 0x0fe8), *font;
-    int k, t;
-
-    if (task->langmode == 0) {
-        for (; *s != 0x00; s++) {
-            putfont8(vram, xsize, x, y, c, hankaku + *s * 16);
-            x += 8;
-        }
-    }
-    if (task->langmode == 1) {
-        for (; *s != 0x00; s++) {
-            if (task->langbyte1 == 0) {
-                if ((0x81 <= *s && *s <= 0x9f) || (0xe0 <= *s && *s <= 0xfc)) {
-                    task->langbyte1 = *s;
-                } else {
-                    putfont8(vram, xsize, x, y, c, nihongo + *s * 16);
-                }
-            } else {
-                if (0x81 <= task->langbyte1 && task->langbyte1 <= 0x9f) {
-                    k = (task->langbyte1 - 0x81) * 2;
-                } else {
-                    k = (task->langbyte1 - 0xe0) * 2 + 62;
-                }
-                if (0x40 <= *s && *s <= 0x7e) {
-                    t = *s - 0x40;
-                } else if (0x80 <= *s && *s <= 0x9e) {
-                    t = *s - 0x80 + 63;
-                } else {
-                    t = *s - 0x9f;
-                    k++;
-                }
-                task->langbyte1 = 0;
-                font = nihongo + 256 * 16 + (k * 94 + t) * 32;
-                putfont8(vram, xsize, x - 8, y, c, font     );  /* ¶¼ª */
-                putfont8(vram, xsize, x    , y, c, font + 16);  /* E¼ª */
-            }
-            x += 8;
-        }
-    }
-    return;
-}
-
 
 void putfont8(char *vram, int xsize, int x, int y, char c, char *font){
     int i;
